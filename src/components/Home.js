f23d0cf75a18d7ab9f216b105024e2f41130f1d8
chyngyzbb <c91618645@gmail.com>
2024-02-28 08:21:45 +0300

    add

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,7 @@ const Home = () => {
             width:'100%',
             height:'550px',
         }}>
+            <h1>Hello</h1>
             <h1>{counter}</h1>
            <button onClick={()=>setCounter(counter+1)}>++</button>
         </div>
